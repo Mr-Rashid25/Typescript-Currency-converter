957382e49bdab572bfe7d592a816430dbc6ac9fb
Rashid. <160320931+Mr-Rashid25@users.noreply.github.com>
2024-05-22 19:50:27 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Rashid Currency Converter
+Rashid Currency Converter is a simple command-line tool built using TypeScript, Inquirer, and Chalk. The main purpose of this program is to convert an amount from one currency to another based on predefined exchange rates. The main currency used for conversion is USD (United States Dollar).
+
+This project uses the following libraries:
+
+* Inquirer for user prompts.
+* Chalk for colored console output.# Typescript-Currency-converter
---
+++ main.ts
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(
+  chalk.bold.greenBright.bgGray(
+    "\n\t>>>>>>>>>> Wellcome To Rashid Currency Converter <<<<<<<<<<"
+  )
+);
+console.log(chalk.redBright("\nNote: Base currency is USD."));
+
+const currency: any = {
+  USD: 1, // main US Dollar
+  EUR: 0.93, // Euro
+  KWD: 0.31, // Kuwaiti Dinar
+  BHD: 0.38, // Bahraini Dinar
+  PKR: 280, // Pakistani Rupees
+};
+let UserAnswer = await inquirer.prompt([
+  {
+    name: "From",
+    message: chalk.blueBright("Select Currency to convert From :"),
+    type: "list",
+    choices: ["USD", "EUR", "KWD", "BHD", "PKR"],
+  },
+  {
+    name: "To",
+    message: chalk.bold.blueBright("Select Currency To convert into :"),
+    type: "list",
+    choices: ["USD", "EUR", "KWD", "BHD", "PKR"],
+  },
+  {
+    name: "Amount",
+    message: chalk.greenBright.bold("Enter Your Amount please:"),
+    type: "number",
+    choices: ["USD", "EUR", "KWD", "BHD", "PKR"],
+  },
+]);
+let userfromCurrency = UserAnswer.From;
+let userToCurrency = UserAnswer.To;
+let fromAmount = currency[userfromCurrency];
+let toAmount = currency[userToCurrency];
+let Amount = UserAnswer.Amount;
+let baseAmount = Amount / fromAmount;
+let convertAmount = baseAmount * toAmount;
+
+console.log(chalk.bold.green(`From ${userfromCurrency}, Currency rate: ${fromAmount}`));
+console.log(chalk.bold.magenta(`To ${userToCurrency}, Currency rate: ${toAmount}`));
+console.log(chalk.bold.cyanBright(`Entered Amount to convert : ${Amount}`));
+console.log(
+  chalk.bold.yellowBright.bgGray(
+    `Converted Amount : ${convertAmount.toFixed(2)}  ${chalk.greenBright(
+      userToCurrency
+    )}`
+    
+  )
+);
